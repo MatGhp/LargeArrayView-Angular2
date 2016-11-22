f85dce1be684268c27032d89f78f74a41553dce5
Mojtaba Ghp <mat.ghp@gmail.com>
2016-11-22 12:23:22 +0100

    animation added to delete item

--- src/app/components/grid.component.ts
+++ src/app/components/grid.component.ts
@@ -1,16 +1,46 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Component,
+  OnInit, Input,
+  Output,
+  EventEmitter,
+  trigger,
+  state,
+  style,
+  transition,
+  animate} from '@angular/core';
 import {ISampleClass} from "../shared/models";
 
 @Component({
   selector: 'lva-grid',
   templateUrl: 'grid.component.html',
-  styles: [``]
-  //to improve the performance in such a scenario, it's recommended to use OnPush strategy
-  //changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [``],
+  animations: [
+    trigger('gridAnimation', [
+      // state('in', style({
+      //   opacity: 1,
+      //   transform: 'translateX(0)'
+      // })),
+      // transition('void => *', [
+      //   style({
+      //     opacity: 0,
+      //     transform: 'translateX(-100px)'
+      //   }),
+      //   animate(300)
+      // ])
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ])
+
+]
+
 })
 export class GridComponent implements OnInit {
 @Input() datasource : ISampleClass[] = [];
 @Output() onDelete = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
